b64ef414ecbfff4478f22161423a3b2631d8321f
QiviPC <igorczeczot@windowslive.com>
2019-03-03 18:10:52 +0100

    17:10 03.03

--- TrainIris.py
+++ TrainIris.py
@@ -2,6 +2,7 @@ from perceptron import Perceptron
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
 
@@ -27,3 +28,25 @@ plt.plot(range(1, len(ppn.errors_) +1), ppn.errors_, marker='o')
 plt.xlabel('Epoki')
 plt.ylabel('Liczba aktualizacji')
 plt.show()
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # konfiguruje generator znacznikow i mape kolorow
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # rysuje wykres powierzchni decyzyjnej
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() +1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() +1
+    xx1, xx2 = np.meshgrid(np.arrange(x1_min, x1_max, resolution),
+                            np.arrange(x2_min,x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # rysuje wykres probek
+
+    
