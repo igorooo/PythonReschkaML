c6378d0b354e269b912392d997e8a07e0a21c793
QiviPC <igorczeczot@windowslive.com>
2019-03-03 16:11:32 +0100

    15:11 03.03

--- TrainIris.py
+++ TrainIris.py
@@ -1,5 +1,29 @@
 from perceptron import Perceptron
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
-print(df.tail())
+
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+
+""""
+plt.scatter(X[:50, 0], X[:50, 1],
+            color ='red', marker ='o', label='Setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='Versicolor')
+plt.xlabel('Dlugosc dzialki [cm]')
+plt.ylabel('Dlugosc platka [cm]')
+plt.legend(loc ='upper left')
+plt.show()
+"""
+
+ppn = Perceptron(eta=0.1, n_iter =10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) +1), ppn.errors_, marker='o')
+plt.xlabel('Epoki')
+plt.ylabel('Liczba aktualizacji')
+plt.show()
